9e3e175871c4aafd6d2d64a8c66254a3815fb248
yuki tachi <qwer_9981@yahoo.co.jp>
2025-05-05 07:51:27 +0900

    base URLとリソースパスをGitHub Pages用に更新

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/travel-blog-sample/', // GitHub Pages 用のベース URL
   plugins: [svelte()],
 })
