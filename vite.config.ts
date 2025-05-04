5564f4eca239d2782e3fb5ea403cdde1e85f0c63
yuki tachi <qwer_9981@yahoo.co.jp>
2025-05-05 07:52:49 +0900

    path変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/travel-blog-sample/', // GitHub Pages 用のベース URL
+  base: '/travel-blog-sample/dist/', // GitHub Pages 用のベース URL
   plugins: [svelte()],
 })
